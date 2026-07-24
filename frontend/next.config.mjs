69ac02bad4a9744b4484e452623aae57823fc8c1
Ahmad Fauzidan <zidankhainur2@gmail.com>
2026-07-24 16:45:42 +0700

    fix(security): update Next.js to latest release and clean next.config.mjs for Vercel deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.geojson$/,
-      loader: "json-loader",
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
